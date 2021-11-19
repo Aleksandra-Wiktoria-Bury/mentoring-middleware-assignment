8ad408551447ae8986ca90ca243363f204986b87
Aleksandra Bury <aleksandra.w.bury@gmail.com>
2021-11-19 16:45:31 +0100

    middleware updated

--- Middleware/doesExisit.js
+++ Middleware/doesExisit.js
@@ -2,45 +2,15 @@ const { body, validationResult } = require("express-validator");
 
 const doesExist = [
   body("firstName").exists().withMessage("First Name is missing"),
-  (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json(errors);
-    }
-    next();
-  },
-    body("lastName").exists().withMessage("Last Name is missing"),
-  (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json(errors);
-    }
-    next();
-  },
+  body("lastName").exists().withMessage("Last Name is missing"),
   body("age").exists().withMessage("Age is missing"),
-  (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json(errors);
-    }
-    next();
-  },
   body("fbw").exists().withMessage("Fbw is missing"),
-  (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json(errors);
-    }
-    next();
-  },
   body("email").exists().withMessage("Email is missing"),
-  (req, res, next) => {
+ (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json(errors);
     }
-    next();
-  },
-];
-
+    next();}
+  ]
 module.exports = doesExist;
